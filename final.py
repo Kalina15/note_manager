452f0b96ae2fdefc38304e842f0a316f85c7cb18
Kalina15 <132050757+Kalina15@users.noreply.github.com>
2024-12-17 23:28:12 +0500

    Add files via upload

---
+++ final.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+temp_created_date = datetime.today()
+
+note = {}
+
+# Создаем словарь для хранения информации
+note["username"] = input("Введите имя пользователя: ")
+note["content"] = input("Введите описание заметки: \n")
+note["status"] = input("Введите статус заметки (например, 'Активна', 'Выполнена'): ")
+
+note["created_date"] = datetime.strftime(temp_created_date, "%d.%m")
+
+temp_issue_date = input("Дата дэдлайна в формате дд.мм.гггг - ")
+note["formatted_issue_date"] = datetime.strptime(temp_issue_date, "%d.%m.%Y").strftime("%d.%m")
+
+
+# Создаем список заголовков заметки
+note["titles"] = []
+for i in range(3):
+    title = input(f"Введите заголовок заметки {i + 1}: ")
+    note["titles"].append(title)
+
+
+# Выводим собранные данные
+print("\nСобранная информация о заметке:")
+for key, value in note.items():
+    print(f"{key.capitalize()}: {value}")
+
